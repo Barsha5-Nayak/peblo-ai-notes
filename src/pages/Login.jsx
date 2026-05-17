634fe3c35fc71f7b39d5abcb5ac2d74dc0c29e3c
Barsha5-Nayak <nayakbarshanayk@gmail.com>
2026-05-17 18:25:02 +0530

    fix login navigation to dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className="auth-container">
       <div className="auth-left">
@@ -15,21 +17,19 @@ const Login = () => {
         <div className="auth-card">
           <h2>Welcome Back</h2>
 
-          <form>
-            <input
-              type="email"
-              placeholder="Enter your email"
-            />
+          <form onSubmit={(e) => {
+              e.preventDefault();
+              navigate("/dashboard");
+            }}>
+            <input type="email" placeholder="Enter your email" />
 
-            <input
-              type="password"
-              placeholder="Enter your password"
-            />
+            <input type="password" placeholder="Enter your password" />
 
             <button type="submit">
-              Login
-            </button>
+           Login
+           </button>
           </form>
+          
 
           <p className="bottom-text">
             Don’t have an account?
